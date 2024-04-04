987f3dd5ceefa85f801ca9e4006fd72a2bb059af
arrangedcupid0 <mstoll3@uwyo.edu>
2024-04-04 16:21:47 -0600

    fuckyowqaodfhnkjsdvjhuaerishd

--- bruteforce.py
+++ bruteforce.py
@@ -1,15 +1,22 @@
 import hashlib
 
 words = open('abridged_rockyou.txt', 'r')
-rockyou = words.read()
+rockyou = words.readlines()
+for i in range(len(rockyou)):
+  rockyou[i] = rockyou[i].strip('\n')
 
 shapass = open('mstoll3-shapass', 'r')
 passwd = shapass.readline()
-print(passwd)
+#print(passwd)
+
+writer = open("output.txt", "w")
 
 for string in rockyou:
-    print(string)
+    #print(string)
     hashed = hashlib.sha256(bytes(string, 'utf-8'))
+    #print(hashed.hexdigest())
+    writer.write(hashed.hexdigest() + "\n")
     if(hashed.hexdigest() == passwd):
         print("password is " + string)
         break
+writer.close()
